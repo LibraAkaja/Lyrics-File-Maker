bc011b2dc04d907e25cb670b230df50d0df08daf
LibraAkaja <adhikaribiraj908@gmail.com>
2025-02-01 13:18:24 +0545

    Add audio play feature

---
+++ main.js
@@ -0,0 +1,29 @@
+document.querySelector("#iconContainer").addEventListener("click", function() {
+    document.querySelector("#a").click();
+});
+
+document.querySelector("#a").addEventListener("change", function() {
+    if(this.files.length > 0){
+        let file = this.files[0];
+        let fileURL = URL.createObjectURL(file);
+        document.querySelector("#fileName").textContent = file.name;
+        let audioElement = document.querySelector("#aud");
+        audioElement.src = fileURL;
+        audioElement.load();
+    }
+});
+
+function playAudio(){
+    const icon = document.querySelector("#pIcon");
+    const a = document.querySelector("#aud");
+    if(icon.src.includes("play.svg")){
+        icon.src = "Assets/pause.svg";
+        a.play().catch((error) => {
+            console.error("Error playing audio: ",error);
+        });
+    }
+    else{
+        icon.src = "Assets/play.svg"
+        a.pause();
+    }
+}
